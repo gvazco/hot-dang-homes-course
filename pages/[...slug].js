2ddd36ca515e9b9e5427708f8a751ded87fe6078
gvazco <gustavovazco@gmail.com>
2023-03-31 19:01:07 -0600

    📦 NEW: init project and first steps

---
+++ pages/[...slug].js
@@ -0,0 +1,66 @@
+import { gql } from "@apollo/client";
+import client from "client";
+import { BlockRenderer } from "components/BlockRenderer";
+import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks";
+
+export default function Page(props) {
+  console.log("PAGE_PROPS: ", props);
+  return (
+    <div>
+      <BlockRenderer blocks={props.blocks} />
+    </div>
+  );
+}
+
+export const getStaticProps = async (context) => {
+  console.log("CONTEXT: ", context);
+  const uri = `/${context.params.slug.join("/")}/`;
+  console.log("URI: ", uri);
+  const { data } = await client.query({
+    query: gql`
+      query PageQuery($uri: String!) {
+        nodeByUri(uri: $uri) {
+          ... on Page {
+            id
+            title
+            blocks
+          }
+        }
+      }
+    `,
+    variables: {
+      uri,
+    },
+  });
+
+  const blocks = cleanAndTransformBlocks(data.nodeByUri.blocks);
+  return {
+    props: {
+      title: data.nodeByUri.title,
+      blocks,
+    },
+  };
+};
+
+export const getStaticPaths = async () => {
+  const { data } = await client.query({
+    query: gql`
+      query AllPagesQuery {
+        pages {
+          nodes {
+            uri
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    paths: data.pages.nodes.map((page) => ({
+      params: {
+        slug: page.uri.substring(1, page.uri.lenght - 1).split("/"),
+      },
+    })),
+    fallback: false,
+  };
+};
